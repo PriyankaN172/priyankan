59df5939c722a0c403b1835e9befc3352876c58a
PriyankaN172 <priyankanxworkz@gmail.com>
2021-06-17 22:12:51 +0530

    sending file

---
+++ Laptop.java
@@ -0,0 +1,11 @@
+class Laptop{
+public static void main(String a[]){
+  String    name    ="Laptop";
+  int          price     =30000;
+  String      version  ="windows7";
+System.out.println(name);
+System.out.println(price);
+System.out.println(version);
+   
+}
+}
