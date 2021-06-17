f1d5ad75982b3cd5952bd3b6a82f9866941e017f
PriyankaN172 <priyankanxworkz@gmail.com>
2021-06-17 21:11:18 +0530

    Technologies file

---
+++ Technologies.java
@@ -0,0 +1,15 @@
+class Technologies
+{
+public static void main(String a[])
+{
+createTechnologies("YouTube",2005);
+createTechnologies("Skype",2003);
+createTechnologies("Wikipedia",2003);
+createTechnologies("Robotics",2000);
+}
+public static void createTechnologies(String technologiesName,int year)
+{
+System.out.println(technologiesName);
+System.out.println(year);
+}
+}
